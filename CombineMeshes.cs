ca78d1fe405d59d2ad0882cda939667e83beec55
Genso-0 <44528817+Genso-0@users.noreply.github.com>
2021-06-04 09:50:25 +0200

    Update CombineMeshes.cs
    Added ability to save mesh and improved naming.

--- CombineMeshes.cs
+++ CombineMeshes.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 /// </summary>
 public class CombineMeshes : MonoBehaviour
 { 
+    public int count;// used for naming the combined objects
     [ContextMenu("Combine mesh")]
     ///Use to combine meshes usin THE SAME material together.
     public void Combine()
     {
+        //make sure obj is at world center.
+        transform.position = Vector3.zero; 
+    
         List<CombineInstance> combinedMeshList = new List<CombineInstance>();
         //Get all meshfilters from this currentObject and its children. Include inactive.
         MeshFilter[] meshFilters = gameObject.GetComponentsInChildren<MeshFilter>(true); 
@@ -28,10 +32,29 @@ public class CombineMeshes : MonoBehaviour
         combinedMesh.CombineMeshes(combinedMeshList.ToArray());
 
         //Create new game object that will hold the new combined mesh.
-        var newObj = new GameObject($"Combined {name}");
-       
+        var newObj = new GameObject($"Combined {name.Replace("Generator","")} {count}");
+        //Need to save mesh to assets.
+        SaveMesh(combinedMesh, newObj.name, false, true);
+        
         newObj.AddComponent<MeshFilter>().mesh = combinedMesh;
         newObj.AddComponent<MeshRenderer>().material = meshFilters[0].GetComponent<MeshRenderer>().sharedMaterial;
         newObj.transform.position += new Vector3(combinedMesh.bounds.size.x, 0, combinedMesh.bounds.size.z);
+        
+        count++;
+    }
+    public static void SaveMesh(Mesh mesh, string name, bool makeNewInstance, bool optimizeMesh)
+    {
+        string path = EditorUtility.SaveFilePanel("Save Separate Mesh Asset", "Assets/", name, "asset");
+        if (string.IsNullOrEmpty(path)) return;
+
+        path = FileUtil.GetProjectRelativePath(path);
+
+        Mesh meshToSave = (makeNewInstance) ? Object.Instantiate(mesh) as Mesh : mesh;
+
+        if (optimizeMesh)
+            MeshUtility.Optimize(meshToSave);
+
+        AssetDatabase.CreateAsset(meshToSave, path);
+        AssetDatabase.SaveAssets();
     }
 }
